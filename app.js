1ae59d7979a97b8ae812d5166987ae91d72d59f1
Balbader <15971200+Balbader@users.noreply.github.com>
2019-12-01 11:18:37 -0500

    Update app.js

--- app.js
+++ app.js
@@ -45,23 +45,19 @@ const budgetController = (function () {
         addItem : function (type, des, val) {
             let newItem;
             let ID;
-            // CREATE NEW ID:
             if (data.allItems[type].length > 0) {
                 ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
             }
             else {
                 ID = 0;
-            }
-            // CREATE NEW ITEM BASED ON 'inc' OR 'exp' type:
+            }          
             if (type === 'exp') {
                 newItem = new Expense(ID, des, val);
             }
             else if (type === 'inc') {
                 newItem = new Income(ID, des, val);
-            }
-            // PUSH IT INTO OUR DATA STRUCTURE:
-            data.allItems[type].push(newItem);
-            // RETURN THE NEW ELEMENT:
+            }         
+            data.allItems[type].push(newItem);          
             return newItem;
         },
         deleteItem : function (type, id) {
@@ -250,36 +246,23 @@ const controller = (function (budgetCtrl, UICtrl) {
         document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
     };
     const updateBudget = function () {
-        // 5.a) Calculate the budget
         budgetCtrl.calculateBudget();
-        // 5.b) Return the budget
         const budget = budgetCtrl.getBudget();
-        // 6. Display the budget on the UI
         UICtrl.displayBudget(budget);
     };
     const updatePercentages = function () {
-        // 1. Calculate the percentages
         budgetCtrl.calculatePercentages();
-        // 2. Read percentages from the 'BUDGET CONTROLLER' modules
         const percentages = budgetCtrl.getPercentages();
-        // 3. Update the UI with the new percentages
         UICtrl.displayPercentages(percentages);
     };
     const ctrlAddItem = function () {
         let newItem;
-        // 1. Get the field input data
         const input = UICtrl.getInput();
         if (input.description !== '' && !isNaN(input.value) && input.value > 0) {
-            // 2. Add the item to the budget controller
             newItem = budgetCtrl.addItem(input.type, input.description, input.value);
-            // 3. Add the item to the UI
             UICtrl.addListItem(newItem, input.type);
-            // 4. Clear the fields
             UICtrl.clearFields();
-            // 5. Calculate and update budget (we call the function
-            // we created with our tasks 5 and 6)
             updateBudget();
-            // 6. Calculate and update percentages
             updatePercentages();
         }
     };
@@ -292,13 +275,9 @@ const controller = (function (budgetCtrl, UICtrl) {
             splitID = itemID.split('-');
             type = splitID[0];
             ID = parseInt(splitID[1]);
-            // 1. delete the item from the data structure
             budgetCtrl.deleteItem(type, ID);
-            // 2. Delete the item from the UI
             UICtrl.deleteListItem(itemID);
-            // 3. Update and show the new budget
             updateBudget();
-            // 4. Calculate and update percentages
             updatePercentages();
         }
     };
